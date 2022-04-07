33a3e480980e9f7cc098855fe883b3320e6ced52
JohanAndres19 <johanandresa@hotmail.com>
2022-04-07 15:46:01 -0500

    proyecto sistemas operativos

--- Logica.py
+++ Logica.py
@@ -1,31 +1,179 @@
-from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtCore import QThread, pyqtSignal, QObject
 import time
 #----------------------------------------
-class CPU:
-    def __init__(self):    
+
+class CPU(QObject):
+    colamayorp=pyqtSignal(bool,int)
+    def __init__(self):
+        super().__init__()    
         self.cola_srt=Cola_Srt(1)
         self.cola_round=Cola_Round(2)
         self.cola_fifo= Cola_FIFO(3)
-        self.command_srt=ShortRafaga(self.cola_srt)
-        self.command_round=Round_robin(self.cola_round)
-        self.command_fifo=Command_FIFO(self.cola_fifo)
+        self.command_srt=ShortRafaga(self.cola_srt,self)
+        self.command_round=Round_robin(self.cola_round,self)
+        self.command_fifo=Command_FIFO(self.cola_fifo,self)
+        self.Tiempocomienzo=0
         self.hiloactual=None
-        
+        #-------------------------------------------------------
+        #-------------------------------------------------------
+        self.command_srt.cambiarP.connect(self.ejecutar_MayorP)
+        self.command_round.cambiarP.connect(self.ejecutar_MayorP)
+        self.command_fifo.cambiarP.connect(self.ejecutar_MayorP)
+        self.command_srt.siguiente.connect(self.ejecutar_Siguiente)
+        self.command_round.siguiente.connect(self.ejecutar_Siguiente)
+        self.command_srt.senal_actualizarespera.connect(self.Actualizar_Espera)
+        self.command_round.senal_actualizarespera.connect(self.Actualizar_Espera)
+        self.fila=0
+        self.ejecutado=False
+
     def Ejecutar(self):
+        self.ejecutado=True
         self.command_srt.start()
+
+    def Mayorprioridad(self,prioridad):
+        if prioridad==1:
+            return self.command_srt
+        elif prioridad ==2:
+            return self.command_round
+        elif prioridad ==3:
+            return self.command_fifo
+        else:
+            return None
+
+    def ejecutar_MayorP(self,prioridad1,prioridad2):
+        if prioridad1==0 and prioridad1!=prioridad2 and prioridad1<prioridad2:
+            self.Mayorprioridad(prioridad2).terminate()
+            self.Mayorprioridad(prioridad1).start()
+    
+    def Agregar(self,opcion,proceso):
+        if opcion==1:
+            self.command_srt.cola.Add_Componente(proceso)
+        elif opcion==2:
+            self.command_round.cola.Add_Componente(proceso)
+        elif opcion==3:
+            self.command_fifo.cola.Add_Componente(proceso)
+        self.colamayorp.emit(True,opcion)
+
+
+    def Actualizar_Espera(self,prioridad):
+        if prioridad==1:
+            self.command_round.LLenarEspera()
+            self.command_fifo.LLenarEspera()
+        elif prioridad==2:
+            self.command_fifo.LLenarEspera()    
+
+    def ejecutar_Siguiente(self,prioridad):
+        if prioridad==1:
+            self.command_round.start()
+        elif prioridad==2:
+            self.command_fifo.start()  
+
+    def Get_Fila(self):
+        return self.fila
+
+    def Set_Fila(self,valor):
+        self.fila=valor
+
+    def Set_Tiempocomienzo(self,valor):
+         self.Tiempocomienzo=valor
+
+    def Get_Tiempocomienzo(self):
+        return self.Tiempocomienzo
+    
 
 #----------------------------------------------
 class Proceso:
-    def __init__(self):
-        print()
+    def __init__(self,nombre,tiempollegada,rafaga):
+        self.Nombre=nombre
+        self.tiempollegada=tiempollegada
+        self.rafaga=rafaga
+        self.prioridad=0
+        self.tiempocomienzo=0
+        self.tiempofinal=0
+        self.tiemporetorno=0
+        self.tiempoespera=0
+        self.isbloqueado=False
+        self.indice=0
+        self.rafaga_ejecutada=0
+
+    def Set_isbloqueado(self,booleano):
+        self.isbloqueado=booleano
+
+    def Get_isbloqueado(self):
+        return self.isbloqueado            
+
+    def Get_nombre(self):
+        return self.Nombre
+
+    def Get_tiempollegada(self):
+        return self.tiempollegada
+
+    def Get_rafaga(self):
+        return self.rafaga        
+
+    def Get_tiempocomienzo(self):
+        return self.tiempocomienzo
+
+    def Get_tiempofinal(self):
+        return self.tiempofinal
+
+    def Get_tiemporetorno(self):
+        return self.tiemporetorno       
+
+    def Get_tiempoespera(self):
+        return self.tiempoespera
+
+    def Get_rafaga_ejecutada(self):
+        return self.rafaga_ejecutada
+
+    def Set_rafaga(self,valor):
+        self.rafaga=valor
+
+    def Set_Prioridad(self,valor):
+        self.prioridad=valor
+
+    def Get_Prioridad(self):
+        return self.prioridad
+
+    def Set_tiempollegada(self,valor):
+        self.tiempollegada=valor
+    def Set_tiempocomienzo(self,tiempo):
+        self.tiempocomienzo=tiempo
+
+    def Set_rafaga_ejecutada(self,valor):
+        self.rafaga_ejecutada=valor
+
+    def Set_tiempofinal(self,tiempo):
+        self.tiempofinal=tiempo
+
+    def Set_tiemporetorno(self,tiempo):
+        self.tiemporetorno=tiempo
+
+    def Set_tiempoespera(self,tiempo):
+        self.tiempoespera=tiempo
+
+    def Get_indice(self):
+        return self.indice
+
+    def Set_indice(self,valor):
+        self.indice=valor
 
 class Cola:
     def __init__(self,prioridad):
         self.lista=[]
         self.prioridad=prioridad
     
+    def Add_Componente(self,proceso):
+        self.lista.append(proceso)
+
+    def Size_cola(self):
+        return len(self.lista)
+
     def isvacia(self):
         return len(self.lista)==0
+    
+    def Get_Prioridad(self):
+        return self.prioridad
 
 class Cola_Srt(Cola):
     def __init__(self, prioridad):
@@ -42,16 +190,54 @@ class Cola_FIFO(Cola):
 #-------------------------------------------
 #----------------------------------------
 class Command(QThread):
-    def __init__(self,cola):
+    cambiarP=pyqtSignal(int,int)
+    senal_actualizar=pyqtSignal(Proceso)
+    siguiente=pyqtSignal(int)
+    senal_actualizarespera=pyqtSignal(int)
+    def __init__(self,cola,cpu):
         super().__init__()
         self.cola=cola
-    
+        self.bloquedo=False
+        self.cpu=cpu
+        self.tiempoenvejecimiento=20
+        self.CambioP=False
+        self.PrioridadActual=0
+        self.cpu.colamayorp.connect(self.CambiarPrioridad)
+      
     def Ejecutar(self):
         pass
 
+    def isbloqueado(self):
+        return self.bloquedo
+    
+    def set_bloqueado(self,valor):
+        self.bloquedo=valor
+
+    def CambiarPrioridad(self,booleano,prioridad):
+        print("cambio la prioridad ",booleano,prioridad)
+        self.CambioP=booleano
+        self.PrioridadActual=prioridad
+
+    def cambioPrioridad(self):
+        return (self.CambioP,self.PrioridadActual)
+    
+    def Agregar(self,proceso):
+        self.cola.Add_Componente(proceso)
+
+    def Completar(self,proceso):
+        proceso.Set_tiempofinal(self.cpu.Get_Tiempocomienzo())
+        proceso.Set_tiemporetorno(proceso.Get_tiempofinal()-proceso.Get_tiempollegada())
+        proceso.Set_rafaga_ejecutada(proceso.Get_rafaga_ejecutada()+(proceso.Get_tiempofinal()-proceso.Get_tiempocomienzo()))
+        self.senal_actualizar.emit(proceso)
+        time.sleep(1)
+    
+    def LLenarEspera(self):
+        for i in self.cola.lista:
+            i.Set_tiempoespera(i.Get_tiempoespera()+1)
+
 class ShortRafaga(Command):
-    def __init__(self, cola):
-        super().__init__(cola)
+    def __init__(self, cola,cpu):
+        super().__init__(cola,cpu)
     
     def Ordenar(self):
         aux=self.cola.lista[0].Get_rafaga()
@@ -65,39 +251,88 @@ class ShortRafaga(Command):
             self.cola.lista.insert(0,elemento)
     
     def Ejecutar(self):
-        while len(self.cola.lista):
+        while self.cola.Size_cola()>0:
+            self.CambiarPrioridad(False, 0)
             self.Ordenar()
             proceso=self.cola.lista.pop(0)
-            while proceso.Get_rafaga()>0 and self.isbloqueado():
+            proceso.Set_tiempocomienzo(self.cpu.Get_Tiempocomienzo())
+            print(proceso.Get_nombre())
+            while proceso.Get_rafaga()>0 and not self.isbloqueado():
+                print(proceso.Get_rafaga())
+                proceso.Set_rafaga(proceso.Get_rafaga()-1)
+                self.LLenarEspera()
+                self.senal_actualizarespera.emit(self.cola.Get_Prioridad())
+                self.cpu.Set_Tiempocomienzo(self.cpu.Get_Tiempocomienzo()+1)                
+                time.sleep(1.5)
+            if self.isbloqueado():
                 pass
-    
+            self.Completar(proceso)
+            valor=self.cambioPrioridad()
+            if valor[0]:
+                self.cambiarP.emit(valor[1],self.cola.Get_Prioridad())
+        self.siguiente.emit(self.cola.Get_Prioridad())    
+
     def run(self):
         self.Ejecutar()
 
 class Round_robin(Command):
-    def __init__(self, cola):
-        super().__init__(cola)
+    def __init__(self, cola,cpu):
+        super().__init__(cola,cpu)
         self.quamtum=4
     
     def Ejecutar(self):
-        while len(self.cola.lista)>0:
+        while self.cola.Size_cola()>0:
+            self.CambiarPrioridad(False, 0)
             proceso =self.cola.lista.pop(0)
-            while proceso.Get_rafaga()>0 and self.isbloqueado():
+            proceso.Set_tiempocomienzo(self.cpu.Get_Tiempocomienzo())
+            quamtum=self.quamtum
+            print(proceso.Get_nombre())            
+            while proceso.Get_rafaga()>0 and not self.isbloqueado() and quamtum>0:
+                print(proceso.Get_rafaga())
+                proceso.Set_rafaga(proceso.Get_rafaga()-1)
+                self.LLenarEspera()
+                self.senal_actualizarespera.emit(self.cola.Get_Prioridad())
+                self.cpu.Set_Tiempocomienzo(self.cpu.Get_Tiempocomienzo()+1)                
+                time.sleep(1.5)
+                quamtum-=1
+            self.Completar(proceso)
+            if proceso.Get_rafaga()>0:
+                proceso.Set_tiempoespera(0)
+                self.cola.Add_Componente(proceso)
+            elif self.isbloqueado():
                 pass
-        else:
-            pass
-
+            valor=self.cambioPrioridad()
+            if valor[0]:
+                self.cambiarP.emit(valor[1],self.cola.Get_Prioridad())
+        self.siguiente.emit(self.cola.Get_Prioridad())            
             
     def run(self):
-        print("hilo command round")
-        time.sleep(1)
+        self.Ejecutar()
 
 class Command_FIFO(Command):
-    def __init__(self, cola):
-        super().__init__(cola)
+    def __init__(self, cola,cpu):
+        super().__init__(cola,cpu)
 
+    def Ejecutar(self):
+        while self.cola.Size_cola()>0:
+            self.CambiarPrioridad(False, 0)
+            proceso =self.cola.lista.pop(0)
+            proceso.Set_tiempocomienzo(self.cpu.Get_Tiempocomienzo())           
+            print(proceso.Get_nombre())
+            while proceso.Get_rafaga()>0 and not self.isbloqueado():
+                print(proceso.Get_rafaga())
+                proceso.Set_rafaga(proceso.Get_rafaga()-1)
+                self.LLenarEspera()
+                self.cpu.Set_Tiempocomienzo(self.cpu.Get_Tiempocomienzo()+1)                
+                time.sleep(1.5)
+            if self.isbloqueado():
+                pass
+            self.Completar(proceso)
+            valor=self.cambioPrioridad()
+            if valor[0]:
+                self.cambiarP.emit(valor[1],self.cola.Get_Prioridad())
+        self.cpu.ejecutado=False        
+    
     def run(self):
-        print("hilo command fifo")
-        time.sleep(1)
-
+        self.Ejecutar()
 
--- gui.py
+++ gui.py
@@ -1,19 +1,250 @@
 import sys
-from PyQt5.QtWidgets import *
-from qt_for_python.uic.ventana import *
-from Logica import *
-class Ventana(QMainWindow):
-    def __init__(self):
+from PyQt5.QtGui import QTextOption , QImage, QPainter
+from  PyQt5.QtWidgets import *
+from PyQt5.QtCore import QSize, Qt
+from qt_for_python.uic.ventanaP import *
+from Logica import *;
+import random
+import threading
+#-------------------------------------
+#------------- Interfaz---------------
+
+class Ventana_principal(QMainWindow):
+    def __init__(self,modelo):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.modelo=CPU()
-        self.ui.pushButton.clicked.connect(lambda: self.modelo.Ejecutar())
+        self.modelo=modelo
+        self.controlador=Controlador(self)
+        self.panel_scroll=QScrollArea(self.ui.frame_3)
+        self.panel_scroll.resize(self.ui.frame_3.width(), self.ui.frame_3.height())
+        #--------------------------------------------------------
+        self.lienzo=Lienzo()
+        self.panel_scroll.setStyleSheet("border: 2px solid;\n border-radius:15px;")
+        self.panel_scroll.setWidget(self.lienzo)
+        #--------------------------------------------------------
+        header =["Proceso","T. llegada","Rafaga","T. comienzo","T. final","T. retorno","T. espera"]
+        self.ui.tableWidget.setColumnCount(7)
+        self.ui.tableWidget.verticalHeader().setVisible(False)
+        self.ui.tableWidget.setHorizontalHeaderLabels(header)
+        self.ui.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+        #----------------------------------------------
+        self.ui.tableWidget_2.setColumnCount(7)
+        self.ui.tableWidget_2.verticalHeader().setVisible(False)
+        self.ui.tableWidget_2.setHorizontalHeaderLabels(header)
+        self.ui.tableWidget_2.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+        #----------------------------------------------------------------------
+        self.ui.tableWidget_4.setColumnCount(7)
+        self.ui.tableWidget_4.verticalHeader().setVisible(False)
+        self.ui.tableWidget_4.setHorizontalHeaderLabels(header)
+        self.ui.tableWidget_4.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+        
+
+    def Get_Lienzo(self):
+        return self.lienzo
+    
+    def Get_modelo(self):
+        return self.modelo
+
+class Lienzo(QFrame):
+    
+    def __init__(self):
+        super().__init__()
+        self.resize(1600, 700)
+        self.setStyleSheet("background: white;\n border: 2px solid;\n border-radius:15px;\n")
+        self.image = QImage(self.size(), QImage.Format_RGB32)
+        self.image.fill(Qt.white)
+        self.dibujardiagrama=False
+    
+    def paintEvent(self,evento):
+        painter = QPainter()
+        painter.drawImage(self.rect(),self.image, self.image.rect())
+        if self.dibujardiagrama:
+            imagen=self.image.copy(0,0,self.width(),self.height())
+            painter.drawImage(self.rect(),imagen,imagen.rect())
+
+        
+#--------------------------------------
+#------------controlador---------------
+
+class Controlador():
+    def __init__(self,ventana):
+        self.ventana =ventana
+        self.Eventos()
+
+    def Eventos(self):
+        self.ventana.ui.boton_simular.clicked.connect(lambda: self.ventana.Get_modelo().Simular())
+        self.ventana.ui.boton_bloquear.clicked.connect(lambda:self.ventana.Get_modelo().Bloquear())
+        self.ventana.ui.boton_agregarSrt.clicked.connect(lambda: self.ventana.Get_modelo().AgregarSrt())
+        self.ventana.ui.boton_agregarRound.clicked.connect(lambda : self.ventana.Get_modelo().AgregarRound())
+        self.ventana.ui.boton_agregarFIFO.clicked.connect(lambda : self.ventana.Get_modelo().AgregarFiFO())
+#---------------------------------------
+#---------------Modelo-----------------
+class Modelo ():
+    
+    def __init__(self) :
+        self.ventana = Ventana_principal(self)
+        self.colaIngresado=[]
+        self.colaListo=[]
+        self.algoritmo=CPU()
+        self.algoritmo.command_srt.senal_actualizar.connect(self.Actualizar_tabla1)
+        self.algoritmo.command_round.senal_actualizar.connect(self.Actualizar_tabla2)
+        self.algoritmo.command_fifo.senal_actualizar.connect(self.Actualizar_tabla3)        
+        self.ProcesoActual=None
+        self.ProcesoAnte=None
+        self.fila=0
+        self.tiempo=0
+        self.opcion=0
+        
+    def RafagaRamdon(self):
+        return random.randint(1,14)
+    
+    def AgregarSrt(self):
+        nombre= 'A'+str(self.ventana.ui.tableWidget.rowCount()+1)
+        rafaga= self.RafagaRamdon()
+        proceso=Proceso(nombre,self.algoritmo.Tiempocomienzo,rafaga)
+        proceso.Set_indice(self.ventana.ui.tableWidget.rowCount())
+        if not self.algoritmo.ejecutado:
+            self.algoritmo.command_srt.Agregar(proceso)
+        else:
+            self.algoritmo.Agregar(1,proceso)    
+        self.dibujar_tabla2(proceso,self.ventana.ui.tableWidget)
 
 
+    def AgregarRound(self):
+        nombre= 'B'+str(self.ventana.ui.tableWidget_2.rowCount()+1)
+        rafaga= self.RafagaRamdon()
+        proceso=Proceso(nombre,self.algoritmo.Tiempocomienzo,rafaga)
+        proceso.Set_indice(self.ventana.ui.tableWidget_2.rowCount())
+        if not self.algoritmo.ejecutado:
+            self.algoritmo.command_round.Agregar(proceso)
+        else:
+            self.algoritmo.Agregar(2,proceso)    
+        self.dibujar_tabla2(proceso,self.ventana.ui.tableWidget_2)
+
+    def AgregarFiFO(self):
+        nombre= 'C'+str(self.ventana.ui.tableWidget_4.rowCount()+1)
+        rafaga= self.RafagaRamdon() 
+        proceso=Proceso(nombre,self.algoritmo.Tiempocomienzo,rafaga)
+        proceso.Set_indice(self.ventana.ui.tableWidget_4.rowCount())
+        if not self.algoritmo.ejecutado:
+            self.algoritmo.command_fifo.Agregar(proceso)
+        else:
+            self.algoritmo.Agregar(3,proceso)    
+        
+        self.dibujar_tabla2(proceso,self.ventana.ui.tableWidget_4)
+
+
+    def Agregar(self):
+        self.opcion=self.ventana.ui.comboBox.currentIndex()
+        """
+        if self.ventana.ui.tableWidget.rowCount()==0:
+            nombre= 'A'+str(self.algoritmo.Get_Fila()+1)
+            rafaga= self.RafagaRamdon()
+            proceso=Proceso(nombre,self.algoritmo.Tiempocomienzo,rafaga)
+        elif '*' not in self.ventana.ui.tableWidget.item(self.ventana.ui.tableWidget.rowCount()-1, 0).text():
+            texto=self.ventana.ui.tableWidget.item(self.ventana.ui.tableWidget.rowCount()-1, 0).text()
+            aux=''
+            for i in texto:
+                if i.isnumeric():
+                    aux+=i
+            aux=int(aux)        
+            nombre= 'A'+str(aux+1)
+            rafaga= self.RafagaRamdon()
+            proceso=Proceso(nombre,self.algoritmo.Tiempocomienzo,rafaga)
+        else:
+            nombre= 'A'+str(self.ventana.ui.tableWidget.rowCount())
+            rafaga= self.RafagaRamdon()
+            proceso=Proceso(nombre,self.algoritmo.Tiempocomienzo,rafaga)
+        """
+        nombre= 'A'+str(self.algoritmo.Get_Fila()+1)
+        rafaga= self.RafagaRamdon()
+        proceso=Proceso(nombre,self.algoritmo.Tiempocomienzo,rafaga)
+        proceso.Set_indice(self.algoritmo.Get_Fila())
+        self.algoritmo.Set_Fila(self.algoritmo.Get_Fila()+1)
+        self.algoritmo.Get_Colaingresado().append(proceso)
+        self.dibujar_tabla2(proceso)
+        """
+        self.opcion=self.ventana.ui.comboBox.currentIndex()
+        nombre='A'+str(self.Get_fila()+1)
+        rafaga= self.RafagaRamdon()
+        proceso=Proceso(nombre,self.algoritmo.Tiempocomienzo,rafaga)
+        proceso.Set_indice(self.fila)
+        self.algoritmo.Get_Colaingresado().append(proceso)
+        self.colaIngresado.append(proceso)
+        self.Set_fila(self.Get_fila()+1)
+        self.dibujar_tabla2(proceso)
+        """
+
+    def Simular(self):
+        self.algoritmo.command_srt.start()
+
+    def dibujarSemaforo(self,imagen):
+        pass
+    def  dibujarespera(self):
+        pass
+    
+    def dibujar_tabla2(self,proceso,tabla):
+        if self.ProcesoAnte!=proceso:
+            self.ProcesoAnte=proceso
+            i=proceso.Get_indice()
+            print(proceso.Get_nombre(),"indice i cantida de filas tabla",i,self.ventana.ui.tableWidget.rowCount())
+            tabla.insertRow(i)
+            tabla.setItem(i, 0, QTableWidgetItem(str(proceso.Get_nombre())))
+            tabla.setItem(i, 1, QTableWidgetItem(str(proceso.Get_tiempollegada())))
+            tabla.setItem(i, 2, QTableWidgetItem(str(proceso.Get_rafaga())))
+            tabla.setItem(i, 3 , QTableWidgetItem(str("")))
+            tabla.setItem(i, 4, QTableWidgetItem(str("")))
+            tabla.setItem(i, 5, QTableWidgetItem(str("")))
+            tabla.setItem(i, 6, QTableWidgetItem(str("")))
+            
+
+    def Actualizar_tabla1(self,proceso):
+            i=proceso.Get_indice()
+            print("esta actualizando el proceso",proceso.Get_nombre(),proceso.Get_tiempocomienzo(),proceso.Get_tiempofinal(),proceso.Get_tiemporetorno(),proceso.Get_tiempoespera())
+            self.ventana.ui.tableWidget.setItem(i, 3, QTableWidgetItem(str(proceso.Get_tiempocomienzo())))
+            self.ventana.ui.tableWidget.setItem(i, 4, QTableWidgetItem(str(proceso.Get_tiempofinal())))
+            self.ventana.ui.tableWidget.setItem(i, 5, QTableWidgetItem(str(proceso.Get_tiemporetorno())))
+            self.ventana.ui.tableWidget.setItem(i, 6, QTableWidgetItem(str(proceso.Get_tiempoespera())))
+    
+    def Actualizar_tabla2(self,proceso):
+            i=proceso.Get_indice()
+            print("esta actualizando el proceso",proceso.Get_nombre(),'ti',proceso.Get_tiempocomienzo(),'tf',proceso.Get_tiempofinal(),'tr',proceso.Get_tiemporetorno(),'te',proceso.Get_tiempoespera())
+            self.ventana.ui.tableWidget_2.setItem(i, 3, QTableWidgetItem(str(self.ventana.ui.tableWidget_2.item(i, 3).text())+','+str(proceso.Get_tiempocomienzo())))
+            self.ventana.ui.tableWidget_2.setItem(i, 4, QTableWidgetItem(str(self.ventana.ui.tableWidget_2.item(i, 4).text())+','+str(proceso.Get_tiempofinal())))
+            self.ventana.ui.tableWidget_2.setItem(i, 5, QTableWidgetItem(str(proceso.Get_tiemporetorno())))
+            self.ventana.ui.tableWidget_2.setItem(i, 6, QTableWidgetItem(str(self.ventana.ui.tableWidget_2.item(i, 6).text())+','+str(proceso.Get_tiempoespera())))
+    
+    def Actualizar_tabla3(self,proceso):
+            i=proceso.Get_indice()
+            print("esta actualizando el proceso",proceso.Get_nombre(),proceso.Get_tiempocomienzo(),proceso.Get_tiempofinal(),proceso.Get_tiemporetorno(),proceso.Get_tiempoespera())
+            self.ventana.ui.tableWidget_4.setItem(i, 3, QTableWidgetItem(str(proceso.Get_tiempocomienzo())))
+            self.ventana.ui.tableWidget_4.setItem(i, 4, QTableWidgetItem(str(proceso.Get_tiempofinal())))
+            self.ventana.ui.tableWidget_4.setItem(i, 5, QTableWidgetItem(str(proceso.Get_tiemporetorno())))
+            self.ventana.ui.tableWidget_4.setItem(i, 6, QTableWidgetItem(str(proceso.Get_tiempoespera())))
+    
+    def Dibujar_semaro(self):
+        pass  
+
+    def Bloquear(self):
+        self.algoritmo.Set_isbloqueado(True)
+        self.algoritmo.Get_Estado().senal_dibujar.connect(self.dibujar_tabla2)
+        self.algoritmo.Get_Estado().senal_actualizar.connect(self.Actualizar_tabla)
+        
+
+    def Get_fila(self):
+        return self.fila
+
+    def Set_fila(self,fila):
+        self.fila=fila
+
+    def Get_ventana(self):
+        return self.ventana    
+
+#--------------------------------------
+#---------------Main------------------- 
 if __name__ =="__main__":
     app =QApplication(sys.argv)
-    gui = Ventana()
+    gui = Modelo().Get_ventana()
     gui.show()
     sys.exit(app.exec_())
-
